5f653a257545b9135d14a2a30ade2f19254b373a
전정우 <jjw012551@gmail.com>
2025-03-20 19:31:42 +0900

    03.20 LLM Finish

--- llm/app/api/chat.py
+++ llm/app/api/chat.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel
 from fastapi import APIRouter, HTTPException
 
 from app.services.chat_service import chat_with_user
+from app.services.chat_process import chat_to_template
 
 chat_router = APIRouter()
 
@@ -29,7 +30,6 @@ async def handle_chat_message(request: ChatRequest):
         # elif message == "오늘은 어떻게 해보면 좋을까?":
         #     return {"response": "수면을 개선하기 위해 호흡 운동이나 스트레칭으로 호흡 곤란을 줄이고, 자기 전 주변 환경을 조용하고 어둡게 유지해보세요."}        
         response = chat_with_user(request.message)
-        print("챗봇의 답변: ", response)
         return {"response": response}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
